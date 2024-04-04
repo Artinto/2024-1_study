03dc37fb4b0ae34996eee13adcb467ded0a1dd7c
sj990710 <127752372+sj990710@users.noreply.github.com>
2024-04-04 21:04:11 +0900

    Create [LSJ]올바른_괄호.py

---
+++ [코딩테스트]/[스택_큐]/[올바른 괄호]/[LSJ]올바른_괄호.py
@@ -0,0 +1,12 @@
+def solution(s):
+    answer = True
+    dic = {'(':1,')':-1} # 문자 별 값 설정
+    total = 0
+    s_list = list(s)
+    for char in s_list:
+        total += dic[char] # 반복문 동안
+        if total < 0: # total이 0 아래로 떨어지면 
+            return False # 올바른 짝이 아님
+    if total != 0: # 마지막이 0이 아닐때도
+        return False # 개수가 맞지 않는 것
+    return True #반복문이 다 돌면 짝 맞는것
