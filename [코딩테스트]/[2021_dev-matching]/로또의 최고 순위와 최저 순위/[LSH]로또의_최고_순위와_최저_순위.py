693e704d5d3116b6132b8729d0b076cbe3c44f13
sj990710 <127752372+sj990710@users.noreply.github.com>
2024-04-04 17:44:00 +0900

    Update [LSH]로또의_최고_순위와_최저_순위.py

--- [코딩테스트]/[2021_dev-matching]/로또의 최고 순위와 최저 순위/[LSH]로또의_최고_순위와_최저_순위.py
+++ [코딩테스트]/[2021_dev-matching]/로또의 최고 순위와 최저 순위/[LSH]로또의_최고_순위와_최저_순위.py
@@ -1,7 +1,7 @@
 def solution(lottos, win_nums):
     answer = []
-    ranking = {6:1,  5:2, 4:3, 3:4, 2:5}
-    match_count = sum(1 for num in lottos if num in win_nums and num !=0)
-    zeros_count = lottos.count(0)
+    ranking = {6:1,  5:2, 4:3, 3:4, 2:5} # 맞춘 개수에 따른 등수 설정
+    match_count = sum(1 for num in lottos if num in win_nums and num !=0) # 지워진 숫자를 제외하고 맞춘 개수
+    zeros_count = lottos.count(0) # 지워진 숫자의 개수
     
-    return [ranking.get(match_count+zeros_count,6),ranking.get(match_count,6)]
+    return [ranking.get(match_count+zeros_count,6),ranking.get(match_count,6)] # [최고 순우,최저 순위]
