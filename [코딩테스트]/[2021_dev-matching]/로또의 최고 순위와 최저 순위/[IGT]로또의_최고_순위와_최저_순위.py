4f38938cbcc54c0790e2463ec69750359c8e0d8f
ImGeuntae <84369594+ImGeuntae@users.noreply.github.com>
2024-04-05 17:55:42 +0900

    Update [IGT]로또의_최고_순위와_최저_순위.py

--- [코딩테스트]/[2021_dev-matching]/로또의 최고 순위와 최저 순위/[IGT]로또의_최고_순위와_최저_순위.py
+++ [코딩테스트]/[2021_dev-matching]/로또의 최고 순위와 최저 순위/[IGT]로또의_최고_순위와_최저_순위.py
@@ -1,4 +1,3 @@
 def solution(lottos, win_nums):
     win_num = len(set(lottos) & set(win_nums))
-    max_win_num = win_num + lottos.count(0)
-    return [min(6,7-max_win_num),min(6,7-win_num)]
+    return [min(6, 7-(win_num + lottos.count(0))), min(6, 7-win_num)]
