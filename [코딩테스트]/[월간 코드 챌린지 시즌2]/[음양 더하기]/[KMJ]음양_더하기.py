72f0bd50daf6af9b7dece5749632e90ef69c2711
MinJi Kim (Mindal) <108729047+mjkim0819@users.noreply.github.com>
2024-05-13 13:24:09 +0900

    Update [KMJ]음양_더하기.py

--- [코딩테스트]/[월간 코드 챌린지 시즌2]/[음양 더하기]/[KMJ]음양_더하기.py
+++ [코딩테스트]/[월간 코드 챌린지 시즌2]/[음양 더하기]/[KMJ]음양_더하기.py
@@ -5,3 +5,16 @@ def solution(absolutes, signs):
             absolutes[i] = -1 * absolutes[i]
         answer += absolutes[i]
     return answer
+
+
+___
+
+
+def solution(absolutes, signs):
+    answer = 0
+    for i in range(len(signs)):
+        if signs[i]:
+            answer += absolutes[i]
+        else:
+            answer -= absolutes[i]
+    return answer
