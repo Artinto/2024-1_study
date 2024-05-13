d7a3226273df6ac4a425585dc238a54bcf3e74d0
MinJi Kim (Mindal) <108729047+mjkim0819@users.noreply.github.com>
2024-05-13 13:13:31 +0900

    Create [KMJ]약수의_개수와_덧셈.py

---
+++ [코딩테스트]/[월간 코드 챌린지 시즌2]/[약수의 개수와 덧셈]/[KMJ]약수의_개수와_덧셈.py
@@ -0,0 +1,14 @@
+def solution(left, right):
+    answer = 0
+    for num in range(left, right+1):
+        point = 0
+        for i in range(1, int(num**(1/2)) + 1):
+            if (num % i == 0):
+                point += 1 
+                if ( (i**2) != num) : 
+                    point += 1
+        if point % 2 == 0:
+            answer += num
+        else:
+            answer -= num
+    return answer
