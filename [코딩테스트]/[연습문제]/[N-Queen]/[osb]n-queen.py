af5e25ea919390598b85fef5df3a0e1ffdc2ecd3
Seungbeom oh <osr0912@gmail.com>
2024-06-24 19:59:07 +0900

    Upload: n-queen

--- [코딩테스트]/[연습문제]/[N-Queen]/[osb]n-queen.py
+++ [코딩테스트]/[연습문제]/[N-Queen]/[osb]n-queen.py
@@ -1,1 +1,20 @@
-# 백트래킹 사용
+def solution(n):
+    answer = 0
+    col_pos = [0] * n
+    diag1 = [0] * (2 * n - 1)
+    diag2 = [0] * (2 * n - 1)
+
+    def backtrack(row):
+        nonlocal answer
+        if row == n:
+            answer += 1
+            return
+        for col in range(n):
+            if col_pos[col] or diag1[row + col] or diag2[row - col + n - 1]:
+                continue
+            col_pos[col] = diag1[row + col] = diag2[row - col + n - 1] = 1
+            backtrack(row + 1)
+            col_pos[col] = diag1[row + col] = diag2[row - col + n - 1] = 0
+
+    backtrack(0)
+    return answer
